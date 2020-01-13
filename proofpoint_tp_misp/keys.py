a5d317bd399a7ffab63dee8c62661de154a918e0
th3jiv3r <th3jiv3r@goodlandsecurity.com>
2020-01-13 13:20:43 -0600

    add proofpoint tap api as variable

--- proofpoint_tp_misp/keys.py
+++ proofpoint_tp_misp/keys.py
@@ -2,3 +2,4 @@ misp_url = 'https://#{MISP}/'
 misp_key = '#{API_KEY}' # The MISP auth key can be found on the MISP on the Users page
 misp_verifycert = False
 misp_client_cert = ''
+proofpoint_key = '#{API_KEY}' # The Proofpoint TAP API key
